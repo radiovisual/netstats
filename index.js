eb5c789f2d726317d126f765b300b87da22a33e0
Michael Wuergler <wuergler@gmail.com>
2016-01-25 11:40:03 +0000

    output examples

--- index.js
+++ index.js
@@ -37,21 +37,3 @@ module.exports = function (port, opts) {
 		});
 	});
 };
-
-/* mac
-COMMAND   PID    USER   FD   TYPE             DEVICE SIZE/OFF NODE NAME
-nc      20661 michael    3u  IPv4 0x3b190d9d07c2c3db      0t0  TCP *:8017 (LISTEN)
-nc      21145 michael    3u  IPv4 0x3b190d9d054773db      0t0  TCP *:8017 (LISTEN)
-Python  21221 michael    3u  IPv4 0x3b190d9ceb8dfd7b      0t0  UDP localhost:8017
-*/
-
-/* win
- TCP    0.0.0.0:9000           0.0.0.0:0              LISTENING       5220
- TCP    127.0.0.1:9000         127.0.0.1:62376        ESTABLISHED     5220
- TCP    127.0.0.1:9000         127.0.0.1:62379        ESTABLISHED     5220
- TCP    127.0.0.1:62288        127.0.0.1:9000         TIME_WAIT       0
- TCP    127.0.0.1:62299        127.0.0.1:9000         TIME_WAIT       0
- TCP    127.0.0.1:62376        127.0.0.1:9000         ESTABLISHED     7604
- TCP    127.0.0.1:62378        127.0.0.1:9000         ESTABLISHED     7604
- UDP    127.0.0.1:9000         *:*                                    1240
-*/
